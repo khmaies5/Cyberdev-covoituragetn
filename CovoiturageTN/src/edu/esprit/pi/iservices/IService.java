f66251f83c1d2006ac9be292b3ddcee3f1afbd82
Khmaies <hssen.khmaies@esprit.tn>
2017-02-01 20:11:00 +0100

    add project structure including Datasource class

---
+++ CovoiturageTN/src/edu/esprit/pi/iservices/IService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.esprit.pi.iservices;
+
+import java.util.List;
+
+/**
+ *
+ * @author khmai
+ */
+public interface IService<T, R> {
+
+    void add(T t);
+
+    void delete(R r);
+
+    T findById(R r);
+
+    List<T> getAll();
+}
