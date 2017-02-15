f90157752bff22c6ae62cc336427989506b9da6a
gargouri <rania.gargouri@esprit.tn>
2017-02-15 11:47:30 +0100

    User et Userservice

--- src/edu/esprit/pi/models/User.java
+++ src/edu/esprit/pi/models/User.java
@@ -5,6 +5,7 @@
  */
 package edu.esprit.pi.models;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,11 +13,52 @@ import java.util.List;
  * @author khmai
  */
 public class User {
+ public static int idd;
 
-    private int id;
+    public static int getIdd() {
+        return idd;
+    }
+
+    public static void setIdd(int idd) {
+        User.idd = idd;
+    }
+    public static int etat_compte;
+
+    public static int getEtat_compte() {
+        return etat_compte;
+    }
+
+    public static void setEtat_compte(int etat_compte) {
+        User.etat_compte = etat_compte;
+    }
+
+   
+    
+     private int id;
     private String nom;
+    private String login;
     private String prenom;
-    private List<Annonce> annonces;
+    private String password;
+    private String email;
+    private String cin;
+    private String photo_Profil;
+    
+    private String niv_Experience;
+    private int nbr_Signalisation;
+    private int nbr_Recommandation;
+    private String travail;
+    private String sexe;
+    private Date date_naissance;
+    private String telephone;
+    private String gouvernorat;
+    private int role;
+   // private List<Annonce> annonces;
+    // private List<Vehicule> vehiculess;
+    // private List<Reservation> reservations;
+    // private List<Sujet> sujets;
+      // private List<Avis> avis;
+    
+    
 
     public User() {
     }
@@ -25,15 +67,44 @@ public class User {
         this.id = id;
     }
 
+    public User(int id, String nom, String login, String prenom, String password, String email) {
+        this.id = id;
+        this.nom = nom;
+        this.login = login;
+        this.prenom = prenom;
+        this.password = password;
+        this.email = email;
+    }
+
     public User(int id, String nom, String prenom) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
     }
 
-    public User(String nom, String prenom) {
+    public User(int id, String nom, String prenom, String password, String email, String cin, String photo_Profil, int etat_compte, String niv_Experience, int nbr_Signalisation, int nbr_Recommandation, String travail, String sexe, Date date_naissance, String telephone, String gouvernorat, int role) {
+        this.id = id;
         this.nom = nom;
         this.prenom = prenom;
+        this.password = password;
+        this.email = email;
+        this.cin = cin;
+        this.photo_Profil = photo_Profil;
+        this.etat_compte = etat_compte;
+        this.niv_Experience = niv_Experience;
+        this.nbr_Signalisation = nbr_Signalisation;
+        this.nbr_Recommandation = nbr_Recommandation;
+        this.travail = travail;
+        this.sexe = sexe;
+        this.date_naissance = date_naissance;
+        this.telephone = telephone;
+        this.gouvernorat = gouvernorat;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", password=" + password + ", email=" + email + ", cin=" + cin + ", photo_Profil=" + photo_Profil + ", etat_compte=" + etat_compte + ", niv_Experience=" + niv_Experience + ", nbr_Signalisation=" + nbr_Signalisation + ", nbr_Recommandation=" + nbr_Recommandation + ", travail=" + travail + ", sexe=" + sexe + ", date_naissance=" + date_naissance + ", telephone=" + telephone + ", gouvernorat=" + gouvernorat + ", role=" + role + '}';
     }
 
     public int getId() {
@@ -42,6 +113,14 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
     }
 
     public String getNom() {
@@ -60,18 +139,118 @@ public class User {
         this.prenom = prenom;
     }
 
-    public List<Annonce> getProducts() {
-        return annonces;
+    public String getPassword() {
+        return password;
     }
 
-    public void setProducts(List<Annonce> products) {
-        this.annonces = products;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    @Override
-    public String toString() {
-        return "User{" + "id=" + id + ", nom=" + nom + ", prenom=" + prenom + '}';
+    public String getEmail() {
+        return email;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getPhoto_Profil() {
+        return photo_Profil;
+    }
+
+    public void setPhoto_Profil(String photo_Profil) {
+        this.photo_Profil = photo_Profil;
+    }
+
+   
+    public String getNiv_Experience() {
+        return niv_Experience;
+    }
+
+    public void setNiv_Experience(String niv_Experience) {
+        this.niv_Experience = niv_Experience;
+    }
+
+    public int getNbr_Signalisation() {
+        return nbr_Signalisation;
+    }
+
+    public void setNbr_Signalisation(int nbr_Signalisation) {
+        this.nbr_Signalisation = nbr_Signalisation;
+    }
+
+    public int getNbr_Recommandation() {
+        return nbr_Recommandation;
+    }
+
+    public void setNbr_Recommandation(int nbr_Recommandation) {
+        this.nbr_Recommandation = nbr_Recommandation;
+    }
+
+    public String getTravail() {
+        return travail;
+    }
+
+    public void setTravail(String travail) {
+        this.travail = travail;
+    }
+
+    public String getSexe() {
+        return sexe;
+    }
+
+    public void setSexe(String sexe) {
+        this.sexe = sexe;
+    }
+
+    public Date getDate_naissance() {
+        return date_naissance;
+    }
+
+    public void setDate_naissance(Date date_naissance) {
+        this.date_naissance = date_naissance;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getGouvernorat() {
+        return gouvernorat;
+    }
+
+    public void setGouvernorat(String gouvernorat) {
+        this.gouvernorat = gouvernorat;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+//    public List<Annonce> getProducts() {
+//        return annonces;
+//    }
+
+//    public void setProducts(List<Annonce> products) {
+//        this.annonces = products;
+//    }
 
     @Override
     public int hashCode() {
@@ -94,5 +273,5 @@ public class User {
         }
         return true;
     }
-
+    
 }
--- src/edu/esprit/pi/services/UserService.java
+++ src/edu/esprit/pi/services/UserService.java
@@ -11,8 +11,11 @@ import edu.esprit.pi.technique.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -29,11 +32,23 @@ public class UserService implements IUserService {
 
     @Override
     public void add(User user) {
-        String req = "insert into user (nom,prenom) values (?,?)";
+       String req = "insert into user(nom,prenom,email,password,cin,travail,sexe,date_naissance,telephone,gouvernerat,role,login)values(?,?,?,?,?,?,?,?,?,?,?,?)";
         try {
             ps = connection.prepareStatement(req);
             ps.setString(1, user.getNom());
             ps.setString(2, user.getPrenom());
+            ps.setString(3, user.getEmail());
+            ps.setString(4, user.getPassword());
+             ps.setString(5, user.getCin());
+             ps.setString(6, user.getTravail());
+             ps.setString(7, user.getSexe());
+           //  ps.setDate(8, user.getDate_naissance());
+             ps.setString(9, user.getTelephone());             
+             ps.setString(10, user.getGouvernorat());
+             ps.setInt(11, 1);
+             ps.setString(12, user.getLogin());
+             
+            
             ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
@@ -88,7 +103,54 @@ public class UserService implements IUserService {
 
     @Override
     public User authentication(String login, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        User u=new User();
+          String i="fail";
+          String k;
+        try {
+            String query = "SELECT id,nom,prenom,login,password,role FROM user WHERE user.login ='"+login+"' and user.password='"+password+"'";
+            
+            
+            PreparedStatement ps = connection.prepareStatement(query);
+            //ps.setString(1, s2+"{"+k+"}");
+            //i=s2+"{"+k+"}";
+            ResultSet rs=  ps.executeQuery(query);
+            
+            while (rs.next()) {
+                u.setId(rs.getInt(1));
+                u.setNom(rs.getString(2));
+                u.setPrenom(rs.getString(3));
+                u.setLogin(rs.getString(4));
+                u.setPassword(rs.getString(5));
+                u.setRole(rs.getInt(6));
+                
+                
+                       
+            }
+            return u;
+          
+            
+
+        } catch (SQLException ex) {
+            Logger.getLogger(IUserService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return u;
+    }
+    @Override
+     public boolean RechLogin(String text) {
+         boolean b=false;
+        try {
+            String query = "SELECT * FROM user WHERE user.login ='"+text+"'" ;
+            PreparedStatement ps = connection.prepareStatement(query);
+            ResultSet rs=  ps.executeQuery(query);
+
+            while (rs.next()) {
+                b=true;
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(UserService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return b;
     }
 
 }
