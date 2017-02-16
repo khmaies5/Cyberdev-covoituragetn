eff0ce0e5b3428beb1af32547a5e2a031f5b5db9
Khmaies <hssen.khmaies@esprit.tn>
2017-02-16 15:22:42 +0100

    changing the design of Annonce

--- src/edu/esprit/pi/gui/InterfaceAnnonceController.java
+++ src/edu/esprit/pi/gui/InterfaceAnnonceController.java
@@ -1,8 +1,14 @@
 package edu.esprit.pi.gui;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXDrawer;
 import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.controls.JFXRippler;
+import com.jfoenix.controls.JFXSnackbar;
+import com.jfoenix.controls.JFXSnackbar.SnackbarEvent;
+import com.jfoenix.controls.JFXToggleButton;
+import com.jfoenix.controls.JFXToggleNode;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
@@ -11,6 +17,7 @@ import com.lynden.gmapsfx.javascript.object.GoogleMap;
 import com.lynden.gmapsfx.javascript.object.LatLong;
 import com.lynden.gmapsfx.javascript.object.MapOptions;
 import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
 import com.lynden.gmapsfx.service.directions.DirectionStatus;
 import com.lynden.gmapsfx.service.directions.DirectionsRenderer;
 import com.lynden.gmapsfx.service.directions.DirectionsRequest;
@@ -18,6 +25,15 @@ import com.lynden.gmapsfx.service.directions.DirectionsResult;
 import com.lynden.gmapsfx.service.directions.DirectionsService;
 import com.lynden.gmapsfx.service.directions.DirectionsServiceCallback;
 import com.lynden.gmapsfx.service.directions.TravelModes;
+import com.lynden.gmapsfx.service.geocoding.GeocodingService;
+import edu.esprit.pi.iservices.ControlledScreen;
+import edu.esprit.pi.iservices.IPublicationFavoritesService;
+import edu.esprit.pi.models.Annonce;
+import edu.esprit.pi.models.PublicationFavorite;
+import edu.esprit.pi.models.User;
+import edu.esprit.pi.services.AnonncesService;
+import edu.esprit.pi.services.PublicationFavoritesService;
+import edu.esprit.pi.services.UserService;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -29,23 +45,37 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
-public class InterfaceAnnonceController implements Initializable, MapComponentInitializedListener, DirectionsServiceCallback {
+public class InterfaceAnnonceController implements Initializable, MapComponentInitializedListener, DirectionsServiceCallback,ControlledScreen {
+
+    private GoogleMap map;
+
+    private GeocodingService geocodingService;
+
+    ScreensController screen;
+
+    private MarkerOptions markerOptions;
 
     protected DirectionsService directionsService;
+
     protected DirectionsPane directionsPane;
+    
+   
 
     protected StringProperty from = new SimpleStringProperty();
     protected StringProperty to = new SimpleStringProperty();
 
-    @FXML
+@FXML
     private AnchorPane root;
 
     @FXML
@@ -70,10 +100,25 @@ public class InterfaceAnnonceController implements Initializable, MapComponentIn
     private Label dateDepartLabel;
 
     @FXML
-    private Label tempDepartLabel;
+    private Label placesLabel;
 
     @FXML
-    private Label tempDepartLabel1;
+    private Separator separator;
+
+    @FXML
+    private Label prixLabel1;
+
+    @FXML
+    private Label placesLabel1;
+
+    @FXML
+    private ImageView userImageView;
+
+    @FXML
+    private Label driverNameLabel;
+
+    @FXML
+    private Label experianceLabel;
 
     @FXML
     private ImageView citere1Label;
@@ -88,21 +133,41 @@ public class InterfaceAnnonceController implements Initializable, MapComponentIn
     private ImageView citere4Label;
 
     @FXML
-    private Label drivaerNameLabel;
+    private JFXRippler favoriteToggle;
 
     @FXML
-    private Label experianceNameLabel;
+    private JFXSnackbar snackbar;
 
     @FXML
-    private JFXDrawer drawer;
+    private JFXCheckBox favCheckBox;
+
+    @FXML
+    private Label tempRest;
+
+    @FXML
+    private Label tempRest1;
 
     @FXML
     private JFXHamburger hamburger;
+    
+       @FXML
+    private JFXDrawer drawer;
+  
+
+    Annonce annonces = new Annonce();
+        AnonncesService annService = new AnonncesService();
+
+    @FXML
+    void favCheckBoxOnAction(ActionEvent event) {
+        User user = new User(6);
+addFavorite(user, annonces);
+    }
+    
+    
 
     @FXML
     private void toTextFieldAction(ActionEvent event) {
-        DirectionsRequest request = new DirectionsRequest(from.get(), to.get(), TravelModes.DRIVING);
-        directionsService.getRoute(request, this, new DirectionsRenderer(true, mapView.getMap(), directionsPane));
+        
     }
    
 
@@ -116,14 +181,45 @@ public class InterfaceAnnonceController implements Initializable, MapComponentIn
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         rootP = root;
+                annonces = annService.findById(ScreensFramework.annonceId);
+                
+                System.out.println(annonces);
+                         mapView.addMapInializedListener(this);
+
+
+        fromToLabel.setText(annonces.getLieuDepart()+"->"+annonces.getLieuArriver());
         
+        distanceLabel.setText("Distance: todo");
+        prixLabel.setText(""+annonces.getPrix());
+        dateDepartLabel.setText("Date Depart: "+annonces.getTripDate().toString());
+        
+        placesLabel.setText(""+annonces.getNbrPersonne());
+        User user = new User();
+        UserService serU = new UserService();
+        
+        user = serU.findById(annonces.getCreator().getId());
+        driverNameLabel.setText(user.getNom()+" "+user.getPrenom());
+        experianceLabel.setText("experiance: todo rania");
+        		snackbar.registerSnackbarContainer(root);
+
+        favoriteToggle.setOnMouseClicked((e) -> {
+			//int value = Integer.parseInt(favoriteToggle.getText());
+			if (e.getButton() == MouseButton.PRIMARY) {
+                            
+				snackbar.fireEvent(new JFXSnackbar.SnackbarEvent("user added to favorite ","CLOSE",3000, true,(b)->{snackbar.close();}));
+                                System.out.println("snackbar");
+                                
+			} else if (e.getButton() == MouseButton.SECONDARY) {
+				
+			}
+
+		});
         try {
             VBox box = FXMLLoader.load(getClass().getResource("SidePanelContent.fxml"));
             drawer.setSidePane(box);
         } catch (IOException ex) {
             Logger.getLogger(InterfaceAnnonceController.class.getName()).log(Level.SEVERE, null, ex);
         }
-         mapView.addMapInializedListener(this);
         //to.bindBidirectional(toTextField.textProperty());
         //from.bindBidirectional(fromTextField.textProperty());
         
@@ -142,18 +238,43 @@ public class InterfaceAnnonceController implements Initializable, MapComponentIn
         });
     }
     
+    
+    public void addFavorite(User user,Annonce annonce){
+        
+                      IPublicationFavoritesService pubFavService= new PublicationFavoritesService(); 
+
+
+
+         PublicationFavorite pubFav =new PublicationFavorite (user,annonce);
+
+        pubFavService.add(pubFav);
+        snackbar.fireEvent(new JFXSnackbar.SnackbarEvent("user added to favorite ","CLOSE",3000, true,(b)->{snackbar.close();}));
+        
+    }
+    
      @Override
     public void mapInitialized() {
+        
         MapOptions options = new MapOptions();
 
-        options.center(new LatLong(47.606189, -122.335842))
+        options.center(new LatLong(34.3055732, 11.255412))
                 .zoomControl(true)
-                .zoom(12)
+                .zoom(6)
                 .overviewMapControl(false)
                 .mapType(MapTypeIdEnum.ROADMAP);
         GoogleMap map = mapView.createMap(options);
         directionsService = new DirectionsService();
         directionsPane = mapView.getDirec();
+        DirectionsRequest request = new DirectionsRequest(annonces.getLieuDepart(), annonces.getLieuArriver(), TravelModes.DRIVING);
+        directionsService.getRoute(request, this, new DirectionsRenderer(true, mapView.getMap(), directionsPane));
+        
+       
+    }
+
+    @Override
+    public void setScreenParent(ScreensController screenPage) {
+
+    screen=screenPage;
     }
 
 }
